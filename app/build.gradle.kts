1ba26a7fbebd597d8d3acf878f75ff6a1489727a
llenhub <118155537+MonsTung@users.noreply.github.com>
2024-06-01 21:35:52 +0800

    Firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +39,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
